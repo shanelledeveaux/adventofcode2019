be91f8067cc952c82f56063c00941fa3a9fdd54c
Shanelle Webster <35905708+shanelledeveaux@users.noreply.github.com>
2019-12-01 10:58:27 -0600

    D1P2 Solution

--- D1P2.js
+++ D1P2.js
@@ -1,1 +1,17 @@
+massCalculator = (num) => Math.floor(num / 3) - 2;
 
+tyrannyRocket = (input) => {
+  var sum = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    var currentMass = input[i];
+
+    while(currentMass > 0){
+      currentMass = massCalculator(currentMass)
+      if(currentMass > 0){
+        sum += currentMass
+      }
+    }
+  }
+  return sum;
+}
